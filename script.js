67cf4f8089158e2a2143066e433a9a4b6bafd512
Megumi fuji <knz1209sin@yahoo.co.jp>
2020-10-28 06:04:44 +0900

    hennkou

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
 // このファイルに解答を書いてください。
 // 問題文はindex.htmlにあります。
 
+function answer(){
+
 
 // Q 1
-let msg = document.getElementById("q1-text");
-	console.log(msg.textContent);
-
-// let msg = document.querySelector("#q1-text");
-// console.log(msg .textContent);
-
-
+var q1_text =document.querySelector("#q1-text");
+console.log(q1_text.textContent);
 
 // Q 2
-let messageS = document.getElementById("q2-text")
-messageS.textContent = "大丈夫";
+var q2_text = document.querySelector("#q2-text");
+q2_text.textContent = "こんにちは";
 
 
-// // Q 3
-let messageSpan = document.getElementById("q3-text");
-    console.log(messageSpan);
-    messageSpan.style.color = "blue";
+// Q 3
+var q3_text = document.querySelector("#q3-text");
+q3_text.style.color="blue";
 
 
-// Q 4 (難)
-// 「for of」を使います。
-
-  let elements = document.getElementsByClassName('q4-text');
-  for(i=0;i<elements.length;i++){
-    elements[i].style.color = "#00ff00";
+// Q 4
+  var q4_text = document.querySelectorAll('.q4-text');
+  console.log(q4_text);
+  // 新しく作ったq4に繰り返している間要素をひとつずつ代入する
+  for(let q4 of q4_text){
+  	console.log(q4);
+  	q4.style.color = "blue";
   }
+  // for ofを使った方法。配列の数がいくつか意識せずに繰り返し処理を行える方法
 
 // Q 5
-
-// let message = document.getElementsByClassName("q5-text");
-//     message.style.backgroundColor = "blue";
-
-//     irobako = document.getElementById("box");
-// irobako.style.backgroundColor = "black";
-// }
-
-    let msgP = document.getElementsByClassName('q5-text');
-  for(i=0;i<msgP.length;i++){
-    msgP[i].style.backgroundColor = "#00ff00";
+  var q5_text = document.querySelectorAll('.q5-text');
+  console.log(q5_text);
+  // 新しく作ったq4に繰り返している間要素をひとつずつ代入する
+  for(let q5 of q5_text){
+  	console.log(q5);
+  	q5.style.backgroundColor = "blue";
   }
+}
+
+// ボタンなしのやり方↓
+
+// Q 1
+// let msg = document.getElementById("q1-text");
+// 	console.log(msg.textContent);
+
+
+
+// // Q 2
+// let messageS = document.getElementById("q2-text")
+// messageS.textContent = "大丈夫";
+
+
+// // // Q 3
+
+// let messageSpan = document.getElementById("q3-text");
+//     console.log(messageSpan);
+//     messageSpan.style.color = "blue";
+
+// // Q 4 (難)
+// // Q 4 (難)
+// // 「for of」を使います。
+
+
+//   let elements = document.getElementsByClassName('q4-text');
+//   for(i=0;i<elements.length;i++){
+//     elements[i].style.color = "#00ff00";
+//   }
+
+// // Q 5
+
+//     let msgP = document.getElementsByClassName('q5-text');
+//   for(i=0;i<msgP.length;i++){
+//     msgP[i].style.backgroundColor = "#00ff00";
+//   }
 
 
